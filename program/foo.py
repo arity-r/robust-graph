172cef84028735f19a275c359b98ad63917e0f0e
arity-r <yoshikimethod@hotmail.co.jp>
2016-01-18 23:21:05 +0900

    add essential files

---
+++ program/foo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from __future__ import division, print_function
+import sys
+import networkx as nx
+
+def main():
+	print('Hello %s'%sys.argv[0])
+	sys.exit(0)
+
+if __name__ == '__main__':
+	main()
