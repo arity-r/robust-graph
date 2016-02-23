2a3d7d0cdacac8abbdd293eacd57188dc1326f72
procon <procon@ichinose-motoki-no-MacBook-Pro.local>
2016-02-24 08:39:47 +0900

    add attack result and generate graph

--- attack_eval.py
+++ attack_eval.py
@@ -3,7 +3,7 @@ import sys, pickle
 from netutil import s
 
 S = []
-N_ATTACK = 1
+N_ATTACK = 100
 for i in range(10):
     with open('graph/'+sys.argv[1]+'%02d.pkl'%i, 'rb') as fp:
         G = pickle.load(fp)
